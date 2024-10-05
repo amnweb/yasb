b2456d487cc30df4cdd7e363ba211bcacf448411
amnweb <16545063+forumwt@users.noreply.github.com>
2024-10-05 12:47:03 +0200

    refactor(console): refactor log monitoring to use LogTailer class

--- src/core/console.py
+++ src/core/console.py
@@ -1,12 +1,32 @@
 import os
-import sys
 from os import path
 from pathlib import Path
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QTextEdit, QDialog, QDialogButtonBox
+from PyQt6.QtWidgets import QVBoxLayout, QTextEdit, QDialog, QDialogButtonBox
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import QProcess, Qt
+from PyQt6.QtCore import QThread, pyqtSignal, Qt
 from settings import DEFAULT_CONFIG_DIRECTORY, DEFAULT_LOG_FILENAME
 from datetime import datetime
+
+class LogTailer(QThread):
+    new_line = pyqtSignal(str)
+    
+    def __init__(self, log_file_path):
+        super().__init__()
+        self.log_file_path = log_file_path
+        self._running = True
+
+    def run(self):
+        with open(self.log_file_path, 'r') as file:
+            file.seek(0, os.SEEK_END)
+            while self._running:
+                line = file.readline()
+                if not line:
+                    self.msleep(100)
+                    continue
+                self.new_line.emit(line)
+
+    def stop(self):
+        self._running = False
 
 class WindowShellDialog(QDialog):
     def __init__(self):
@@ -16,17 +36,13 @@ class WindowShellDialog(QDialog):
         icon_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'assets', 'images', 'app_icon.png')
         
         icon = QIcon(icon_path)
-        # Create a pixmap of size 48x48 and set it as the window icon
         self.setWindowIcon(QIcon(icon.pixmap(48, 48)))
- 
-        icon = QIcon(icon_path)
-        self.setWindowIcon(icon)
+
         self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowMinimizeButtonHint)
         self.setWindowModality(Qt.WindowModality.WindowModal)
         self.setContentsMargins(0, 0, 0, 0)
-        # Create a layout for the dialog
+
         layout = QVBoxLayout()
-        # Create a QTextEdit widget for the output
         self.output_viewer = QTextEdit()
         self.output_viewer.setReadOnly(True)
         self.output_viewer.setStyleSheet("""
@@ -48,63 +64,36 @@ class WindowShellDialog(QDialog):
         """)
         layout.addWidget(self.output_viewer)
 
-        # Add a button box for closing the dialog
         button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Close)
         button_box.rejected.connect(self.close_dialog)
         layout.addWidget(button_box)
 
-        # Set the layout for the dialog
         self.setLayout(layout)
 
-        # Create a QProcess to run PowerShell commands
-        self.process = QProcess()
-        self.process.readyReadStandardOutput.connect(self.update_output)
-        self.process.readyReadStandardError.connect(self.update_output)
-        self.process.started.connect(lambda: self.output_viewer.append(f"Log started {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"))
-
-        # Run the PowerShell command
-        self.run_powershell_command()
-
-    def run_powershell_command(self):
         HOME_CONFIGURATION_DIR = path.join(Path.home(), DEFAULT_CONFIG_DIRECTORY)
-        # Run a PowerShell command to watch the log file in real-time
         log_file_path = f"{HOME_CONFIGURATION_DIR}\\{DEFAULT_LOG_FILENAME}"
-        command = f"Get-Content -Path '{log_file_path}' -Tail 10 -Wait"
-        self.process.start("powershell.exe", ["-Command", command])
 
-    def update_output(self):
-        # Update the output viewer with the latest output from the process
-        output = self.process.readAllStandardOutput().data().decode()
-        error = self.process.readAllStandardError().data().decode()
-        
-        if output:
-            self.append_colored_text(output)
-        if error:
-            self.append_colored_text(error)
+        self.log_tailer = LogTailer(log_file_path)
+        self.log_tailer.new_line.connect(self.append_colored_text)
+        self.log_tailer.start()
 
-    def append_colored_text(self, text):
-        # Split the text into lines and apply color formatting
-        for line in text.splitlines():
-            if "CRITICAL" in line:
-                formatted_line = f'<span style="color:#ff000;font-weight:bold">{line}</span>'
-            elif "ERROR" in line:
-                formatted_line = f'<span style="color:#ff4d4d;;font-weight:bold">{line}</span>'
-            elif "WARNING" in line:
-                formatted_line = f'<span style="color:#ffb64d;">{line}</span>'
-            else:
-                formatted_line = f'<span>{line}</span>'
-            self.output_viewer.append(formatted_line)
+        self.output_viewer.append(f"Log started {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+    def append_colored_text(self, line):
+        if "CRITICAL" in line:
+            formatted_line = f'<span style="color:#ff000;font-weight:bold">{line}</span>'
+        elif "ERROR" in line:
+            formatted_line = f'<span style="color:#ff4d4d;;font-weight:bold">{line}</span>'
+        elif "WARNING" in line:
+            formatted_line = f'<span style="color:#ffb64d;">{line}</span>'
+        else:
+            formatted_line = f'<span>{line}</span>'
+        self.output_viewer.append(formatted_line)
 
     def close_dialog(self):
-        # Terminate the PowerShell process when the dialog is closed
-        self.terminate_process()
+        self.log_tailer.stop()
         self.close()
 
     def closeEvent(self, event):
-        # Terminate the PowerShell process when the window is closed
-        self.terminate_process()
+        self.log_tailer.stop()
         event.accept()
-
-    def terminate_process(self):
-        if self.process.state() == QProcess.ProcessState.Running:
-            self.process.kill()
