c7104d93bf0d9fe79fabe4d82a08f33d544aed52
amnweb <16545063+forumwt@users.noreply.github.com>
2024-10-20 22:30:02 +0200

    Update build version to 1.3.0

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,7 @@ APP_BAR_TITLE = "YasbBar"
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 GITHUB_URL = "https://github.com/amnweb/yasb"
 GITHUB_THEME_URL = "https://github.com/amnweb/yasb-themes"
-BUILD_VERSION = "1.2.9"
+BUILD_VERSION = "1.3.0"
 # Development Settings
 DEBUG = False
 # Configuration Settings
