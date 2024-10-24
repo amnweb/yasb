ea44a0bb33e5254be49d87bc83794a3441ab02b0
amnweb <16545063+forumwt@users.noreply.github.com>
2024-10-24 19:35:18 +0200

    refactor(utils): Add is_windows_10() method - Added a new method is_windows_10() to check if the platform is Windows 10.

--- src/core/utils/utilities.py
+++ src/core/utils/utilities.py
@@ -1,19 +1,21 @@
+import platform
+import re
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QScreen
 
-
 # General Utility Methods
+def is_windows_10() -> bool:
+    version = platform.version()
+    return bool(re.match(r'^10\.0\.1\d{4}$', version))
+
 def percent_to_float(percent: str) -> float:
     return float(percent.strip('%')) / 100.0
-
 
 def is_valid_percentage_str(s: str) -> bool:
     return s.endswith("%") and len(s) <= 4 and s[:-1].isdigit()
 
-
 def get_screen_by_name(screen_name: str) -> QScreen:
     return next(filter(lambda scr: screen_name in scr.name(), QApplication.screens()), None)
-
 
 class Singleton(type):
     _instances = {}
