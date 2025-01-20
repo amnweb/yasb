c61399745f60ea70bad91b516928f9cb2309e71f
amnweb <16545063+forumwt@users.noreply.github.com>
2025-01-21 00:53:05 +0100

    chore(cli): bump CLI version to 1.0.6 and clean up uninstall command

--- src/core/utils/cli.py
+++ src/core/utils/cli.py
@@ -19,7 +19,7 @@ from colorama import just_fix_windows_console
 just_fix_windows_console()
 
 YASB_VERSION = BUILD_VERSION
-YASB_CLI_VERSION = "1.0.5"
+YASB_CLI_VERSION = "1.0.6"
 
 OS_STARTUP_FOLDER = os.path.join(os.environ['APPDATA'], r'Microsoft\Windows\Start Menu\Programs\Startup')
 INSTALLATION_PATH = os.path.abspath(os.path.join(__file__, "../../.."))
@@ -354,17 +354,17 @@ class CLIUpdateHandler():
             subprocess.run(["taskkill", "/f", "/im", "yasb_themes.exe"], creationflags=subprocess.CREATE_NO_WINDOW)
 
         # Construct the uninstall command as a string
-        product_code = CLIUpdateHandler.get_installed_product_code()
-        if product_code is not None:
-            uninstall_command = f'msiexec /x {product_code} /passive'
-        else:
-            uninstall_command = ""
+        # product_code = CLIUpdateHandler.get_installed_product_code()
+        # if product_code is not None:
+        #     uninstall_command = f'msiexec /x {product_code} /passive'
+        # else:
+        #     uninstall_command = ""
             
         # Construct the install command as a string
         install_command = f'msiexec /i "{os.path.abspath(msi_path)}" /passive /norestart'
         run_after_command = f'"{EXE_PATH}"'
-        combined_command = f'{uninstall_command} && {install_command} && {run_after_command}'
-
+        #combined_command = f'{uninstall_command} && {install_command} && {run_after_command}'
+        combined_command = f'{install_command} && {run_after_command}'
         # Finally run update and restart the application
         subprocess.Popen(combined_command, shell=True)
         sys.exit(0)
