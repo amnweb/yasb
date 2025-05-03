ae9b56384a7c41dfeed7f073f826da634e2afd56
amnweb <16545063+forumwt@users.noreply.github.com>
2025-05-04 00:38:24 +0200

    chore(settings): update build and CLI version numbers
    - Increment BUILD_VERSION to 1.7.1
    - Increment CLI_VERSION to 1.1.0

--- src/settings.py
+++ src/settings.py
@@ -7,8 +7,8 @@ APP_BAR_TITLE = "YasbBar"
 SCRIPT_PATH = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
 GITHUB_URL = "https://github.com/amnweb/yasb"
 GITHUB_THEME_URL = "https://github.com/amnweb/yasb-themes"
-BUILD_VERSION = "1.7.0"
-CLI_VERSION = "1.0.9"
+BUILD_VERSION = "1.7.1"
+CLI_VERSION = "1.1.0"
 # Development Settings
 DEBUG = False
 # Configuration Settings
