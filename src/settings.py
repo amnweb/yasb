0a8f75de4e15a86babac635219cdfe8417ffaff4
AmN <16545063+amnweb@users.noreply.github.com>
2025-07-29 16:18:14 +0200

    chore(settings): update build version to 1.7.9

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ SCRIPT_PATH = (
 )
 GITHUB_URL = "https://github.com/amnweb/yasb"
 GITHUB_THEME_URL = "https://github.com/amnweb/yasb-themes"
-BUILD_VERSION = "1.7.8"
+BUILD_VERSION = "1.7.9"
 CLI_VERSION = "1.1.2"
 RELEASE_CHANNEL = "stable"
 # Development Settings
