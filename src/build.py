5669a4933465c29fa5739f3b57b108f8a9f9e185
AmN <16545063+amnweb@users.noreply.github.com>
2025-09-12 14:42:12 +0200

    feat(workflow): add GitHub Actions workflow for building and signing executables

--- src/build.py
+++ src/build.py
@@ -121,6 +121,7 @@ bdist_msi_options = {
     "dist_dir": "dist/out",
     "initial_target_dir": r"[ProgramFiles64Folder]\YASB",
     "all_users": True,
+    "skip_build": True,
     "summary_data": {
         "author": "AmN",
         "comments": "A highly configurable Windows status bar",
