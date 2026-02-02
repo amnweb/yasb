edc0b126e9bd5d6d5d62ca6b9b043dff24a5445e
Washik <generalarmyhf@gmail.com>
2026-02-02 01:43:04 +0100

    added-unlimited-icon-option

--- src/core/widgets/yasb/battery.py
+++ src/core/widgets/yasb/battery.py
@@ -98,7 +98,7 @@ class BatteryWidget(BaseWidget):
     def _get_time_remaining(self) -> str:
         secs_left = self._battery_state.time_remaining
         if secs_left == POWER_TIME_UNLIMITED:
-            time_left = "unlimited"
+            time_left = self._status_icons["icon_unlimited"]
         elif secs_left == POWER_TIME_UNKNOWN:
             time_left = "unknown"
         elif isinstance(secs_left, int) and secs_left >= 0:
