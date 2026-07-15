55bdc45ea1c6a6fcc56df79fc7a8ad19ee05f39d
Tamion <70228790+notTamion@users.noreply.github.com>
2026-07-15 10:57:10 +0200

    mic and brightness

--- src/core/widgets/yasb/microphone.py
+++ src/core/widgets/yasb/microphone.py
@@ -252,9 +252,10 @@ class MicrophoneWidget(BaseWidget):
     def wheelEvent(self, event: QWheelEvent):
         if self.audio_endpoint is None:
             return
-        if event.angleDelta().y() > 0:
+        delta = -event.angleDelta().y() if self.config.invert_wheel else event.angleDelta().y()
+        if delta > 0:
             self._increase_volume()
-        elif event.angleDelta().y() < 0:
+        elif delta < 0:
             self._decrease_volume()
 
     def show_menu(self):
