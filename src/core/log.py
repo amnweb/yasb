7d09d5f2b0a219b233a55c2936ba14df3de82155
AmN <16545063+amnweb@users.noreply.github.com>
2026-08-05 20:02:35 +0200

    fix(log): disable c_stack in faulthandler on Windows

--- src/core/log.py
+++ src/core/log.py
@@ -99,8 +99,8 @@ def init_logger():
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(ColoredFormatter(CONSOLE_FORMAT, datefmt=CONSOLE_DATETIME))
     logging.basicConfig(level=logging.DEBUG, handlers=[file_handler, console_handler], encoding="utf-8")
-
-    faulthandler.enable(file=file_handler.stream, all_threads=True, c_stack=True)
+    # c_stack is dead on Windows
+    faulthandler.enable(file=file_handler.stream, all_threads=True, c_stack=False)
     logging.info("%s v%s", APP_NAME, BUILD_VERSION)
 
 
